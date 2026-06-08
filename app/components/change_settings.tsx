9166efbafa88bf88a8f6985c51bee77dab4d8747
NicoGuff <nicogufron@gmail.com>
2026-06-08 16:58:42 +0700

    update:added new setting for default currency

--- app/components/change_settings.tsx
+++ app/components/change_settings.tsx
@@ -4,11 +4,13 @@ import { Button } from "@/components/ui/button";
 import { Field, FieldLabel } from "@/components/ui/field";
 import { Item, ItemContent, ItemDescription, ItemMedia, ItemTitle } from "@/components/ui/item";
 import { Textarea } from "@/components/ui/textarea";
-import { Info } from "lucide-react";
+import { ChevronDownIcon, Info } from "lucide-react";
 import { CompanyData, useCompanyStore } from "../stores/company.store";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { Spinner } from "@/components/ui/spinner";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Command, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
 
 export default function Settings() {
 
@@ -26,7 +28,7 @@ export default function Settings() {
     const handleChange = (key: keyof CompanyData, e: any) => {
         setCompanyTermsAndNotes(prev => ({
             ...prev,
-            [key] : e,
+            [key]: e,
         }));
     }
 
@@ -35,24 +37,70 @@ export default function Settings() {
 
         const res = await saveCompanyUpdate(company.id, companyTermsAndNotes!);
 
-        if (res) toast.success("Changes saved", )
+        if (res) toast.success("Changes saved",)
     }
 
     useEffect(() => {
         setCompanyTermsAndNotes(company!);
     }, [])
 
+    const CURRENCIES = [
+        {
+            value: "$",
+            code: "USD",
+            label: "US Dollar"
+        },
+        {
+            value: "€",
+            code: "EUR",
+            label: "Euro"
+        },
+        {
+            value: "£",
+            code: "GBP",
+            label: "British Pound",
+        },
+        {
+            value: "Rp",
+            code: "IDR",
+            label: "Indonesian Rupiah"
+        }
+    ]
+
     return (
         <div className="py-5">
             <Item variant={"outline"} className="bg-blue-50 w-full ">
                 <ItemMedia><Info size={16}></Info></ItemMedia>
                 <ItemContent>
-                    <ItemTitle>Attention</ItemTitle>
+                    <ItemTitle className="font-bold">Attention</ItemTitle>
                     <ItemDescription className="text-black">Changes will be saved and applied to future invoices automatically</ItemDescription>
                 </ItemContent>
             </Item>
             <div>
                 <div className="flex flex-col gap-5 mt-5">
+
+                    <div className="flex flex-col items-start gap-2.5">
+                        <p className="font-medium text-sm">Set Default Currency</p>
+                        <Popover>
+                            <PopoverTrigger>
+                                <Button variant="outline">$ US Dollar<ChevronDownIcon /></Button>
+                            </PopoverTrigger>
+                            <PopoverContent align="start">
+                                <Command>
+                                    <CommandInput></CommandInput>
+                                    <CommandList>
+                                        <CommandGroup>
+                                            {CURRENCIES.map((c) => (
+                                                <CommandItem key={c.value} onSelect={(e) => {
+
+                                                }}>{c.value + " " + c.label}</CommandItem>
+                                            ))}
+                                        </CommandGroup>
+                                    </CommandList>
+                                </Command>
+                            </PopoverContent>
+                        </Popover>
+                    </div>
                     <Field>
                         <FieldLabel>Notes</FieldLabel>
                         <Textarea rows={3} defaultValue={company?.default_notes} onChange={(e) => handleChange('default_notes', e.currentTarget.value)}></Textarea>
@@ -64,6 +112,6 @@ export default function Settings() {
                 </div>
                 <Button className="mt-5" onClick={handleUpdateCompany}>{isUpdating ? <Spinner></Spinner> : "Save changes"}</Button>
             </div>
-        </div>
+        </div >
     )
 }
